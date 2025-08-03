842b8cf4cc98c8fc9e1ab0fa06ddf35407ce6488
RajaMuruganG <er.raajaa@gmail.com>
2025-08-03 10:53:46 +0530

    Inheritance override and abstraction

--- OopsExamples/src/com/oops/abs/Client.java
+++ OopsExamples/src/com/oops/abs/Client.java
@@ -8,22 +8,23 @@ public class Client {
 		bank.carLoan();
 		bank.homeLoan();
 		bank.personalLoan();
-		
+
 		System.out.println();
-		Branch1 branch1 =(Branch1) bank; //downcasting to branch1
+		Branch1 branch1 = (Branch1) bank; // downcasting to branch1
 		branch1.payDetails();
-		
+
 		System.out.println();
-		
+
+		// create obj of SubBranch and assign to Bank Ref
 		bank = new SubBranch();
 		bank.carLoan();
 		bank.personalLoan();
-		
+
 		System.out.println();
-		
+
 		SubBranch subBranch = (SubBranch) bank;
 		subBranch.depositType();
-		subBranch.adminDetail();// called from subbranch
+		subBranch.adminDetail();// called from subBranch
 		subBranch.loanType();
 	}
 }
